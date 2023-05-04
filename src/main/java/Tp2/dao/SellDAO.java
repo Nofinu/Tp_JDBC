916ae14ce9e259b160218acd6828ff685a01c4e3
Léo Lhomme <leolhommeo@gmail.com>
2023-05-04 17:28:13 +0200

    ajout de la gestion des ventes

--- src/main/java/Tp2/dao/SellDAO.java
+++ src/main/java/Tp2/dao/SellDAO.java
@@ -1,11 +1,14 @@
 package Tp2.dao;
 
+import Tp2.model.Car;
+import Tp2.model.Personne;
 import Tp2.model.Sell;
 import jdk.jshell.spi.ExecutionControl;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SellDAO extends BaseDAO<Sell> {
@@ -31,18 +34,36 @@ public class SellDAO extends BaseDAO<Sell> {
     }
 
     @Override
-    public List<Sell> getAll() throws ExecutionControl.NotImplementedException, SQLException {
-        return null;
+    public List<Sell> getAll() throws SQLException {
+        List<Sell> sells = new ArrayList<>();
+        request = "SELECT sell.id as id_sell,id_personne,id_car,date,name,year,power,price,last_name,first_name,age FROM sell INNER JOIN car ON sell.id_car = car.id INNER JOIN personne ON sell.id_personne = personne.id\n";
+        statement = _connection.prepareStatement(request);
+        resultSet = statement.executeQuery();
+        while (resultSet.next()){
+            Car car = new Car(resultSet.getInt("id_car"),
+                    resultSet.getString("name"),
+                    resultSet.getString("year"),
+                    resultSet.getFloat("power"),
+                    resultSet.getFloat("price"));
+            Personne personne = new Personne(resultSet.getInt("id_personne"),
+                    resultSet.getString("last_name"),
+                    resultSet.getString("first_name"),
+                    resultSet.getInt("age"));
+            Sell sell = new Sell(resultSet.getInt("id_sell"),
+                    personne,car,resultSet.getDate("date"));
+            sells.add(sell);
+        }
+        return sells;
     }
 
     @Override
-    public boolean update(Sell element) throws ExecutionControl.NotImplementedException, SQLException {
-        return false;
+    public boolean update(Sell element) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("sell");
     }
 
     @Override
-    public boolean delete(int id) throws ExecutionControl.NotImplementedException, SQLException {
-        return false;
+    public boolean delete(int id) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("sell");
     }
 
     @Override
